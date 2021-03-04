b4eec1e949964f1c4684fa91db0f5bedc8508677
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-04 17:06:00 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+   public class Customer
+    {
+        //custid,custname,city,state,country,cardno, etc
+        private bool PrimeCustomer;
+        public string ShowCustomerStatus()
+        {
+            string custType = null;
+            if (PrimeCustomer)
+            {
+                custType = "Welcome Prime Customer";
+            }
+            else
+            {
+                custType = "Welcome  Customer";
+            }
+            return custType;
+        }
+
+
+
+        private int _custid;
+        public int CustId
+        {
+            get {
+                
+                
+                return _custid; }
+            set {
+
+                
+
+                if (value == 0)
+                {
+                    throw new Exception("custid cannot be zero");
+                }
+                else
+                {
+                    _custid = value;
+                    if (_custid >= 1 && _custid <= 10)
+                    {
+                        PrimeCustomer = true;
+                    }
+
+                }
+
+
+            }
+        }
+
+        private int _cardno;
+        public int Cardno
+        {
+            get { return _cardno; }
+            set { _cardno = value; }
+        }
+
+
+
+
+
+    }
+}
---
+++ CustomerMgt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLibrary;
+namespace OOPs
+{
+    class CustomerMgt
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer();
+            try
+            {
+
+            
+            Console.WriteLine("Enter ur customer id");
+            c.CustId=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ur card no");
+            c.Cardno = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(c.ShowCustomerStatus());
+
+
+
+
+                Console.WriteLine("U have entered Customer ID=  " + c.CustId);
+
+
+            //Customer c1 = new Customer();
+            //Console.WriteLine("Enter ur customer id");
+            //c1.CustId = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("U have entered Customer ID=  " + c1.CustId);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+
+        }
+    }
+}
