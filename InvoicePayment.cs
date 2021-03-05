6ea243b255de6f8fa5fa2797689bd4724dd31c7b
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-05 11:28:39 +0530

    Add files via upload

---
+++ InvoicePayment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class InvoicePayment
+    {
+        //Cash/Cheque/RTGS/NEFT/UPIID
+        //InvoiceNo/Amt
+
+        public int InvNo { get; set; }
+        public double Amt { get; set; }
+
+        private bool paystatus=false;
+        public bool PaymentStatus { get { return paystatus; } }
+
+        public void Pay(int invno, double amt)
+        {
+            paystatus = true;
+        }
+        public void Pay(int invno, double amt, double chqno, string bankname, string branch)
+        {
+            paystatus = true;
+        }
+        public void Pay(int invno, double amt, string bankname, int nefttranid)
+        {
+            paystatus = true;
+        }
+        public void Pay(int invno, double amt, string bankname, string branch, int rtgstranid)
+        {
+            paystatus = true;
+        }
+
+        public void Pay(int invno, double amt, string upiid)
+        {
+        }
+
+
+
+    }
+}
