2e5eb061285571e6254826018e2572ce95d7ea29
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-05 09:55:46 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+   public class Customer
+    {
+        //custid,custname,city,state,country,cardno, etc
+        private bool PrimeCustomer;
+        public string ShowCustomerStatus()
+        {
+            string custType = null;
+            if (PrimeCustomer)
+            {
+                custType = "Welcome Prime Customer";
+            }
+            else
+            {
+                custType = "Welcome  Customer";
+            }
+            return custType;
+        }
+
+
+
+        private int _custid;
+        public int CustId
+        {
+            get {
+                
+                
+                return _custid; }
+            set {
+                if (value == 0)
+                {
+                    throw new Exception("custid cannot be zero");
+                }
+                else
+                {
+                    _custid = value;
+                    if (_custid >= 1 && _custid <= 10)
+                    {
+                        PrimeCustomer = true;
+                    }
+
+                }
+
+
+            }
+        }
+
+        private long _cardno;
+        public long Cardno
+        {
+            get { return _cardno; }
+            set { _cardno = value; }
+        }
+
+        private string _custname;
+
+        public string CustName
+        {
+            get { return _custname; }
+            set { _custname = value; }
+        }
+
+
+        //private DateTime _birthdate; (propfull + tab+tab)
+
+        //public DateTime BirthDate
+        //{
+        //    get { return _birthdate; }
+        //    set { _birthdate = value; }
+        //} 
+        //_BirthDate
+
+        public DateTime BirthDate { get; set; }
+
+        //Write Only
+        private string _password;
+        public string Password
+        {
+            set { _password = value; }
+        }
+
+        private int GenerateOTP()
+        {
+          Math.DivRem(CustId,10,out int res);
+            return res;
+        }
+
+        //Read only
+        private int _otp;
+        public int OTP
+        {
+            get
+            {
+
+                _otp = GenerateOTP();
+                return _otp;
+            }
+        }
+        //readonly property snippet- propg tab+tab
+        //public int OTP { get
+        //    {
+        //        _otp = GenerateOTP();
+        //        return _otp;
+        //    } private set { _otp = value; } }
+
+
+
+
+
+        private int _CheckOTP;
+        public int CheckOTP
+        {
+            set {
+                if (value==OTP)
+                {
+                    _CheckOTP = value;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect OTP");
+                }
+                
+            }
+        }
+
+        
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CustomerMgt.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLibrary;
+namespace OOPs
+{
+    class CustomerMgt
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer();
+            try
+            {
+
+            
+            Console.WriteLine("Enter ur customer id");
+            c.CustId=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ur card no");
+            c.Cardno = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine(c.ShowCustomerStatus());
+
+                Console.WriteLine("Enter Customer Name");
+                c.CustName = Console.ReadLine();
+                Console.WriteLine("Enter Birthday Date");
+                c.BirthDate = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Enter password");
+                c.Password = Console.ReadLine();
+
+                Console.WriteLine("Your OTP= " + c.OTP);
+                Console.WriteLine("Please enter ur OTP here");
+                c.CheckOTP = Convert.ToInt32(Console.ReadLine());
+                c.OTP = 1234;             
+
+                Console.WriteLine("U have entered Customer ID=  " + c.CustId);
+                Console.WriteLine("Customer Name " + c.CustName);
+                Console.WriteLine("BirthDate " + c.BirthDate);
+                Console.WriteLine("Card No" + c.Cardno);
+
+
+
+            //Customer c1 = new Customer();
+            //Console.WriteLine("Enter ur customer id");
+            //c1.CustId = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("U have entered Customer ID=  " + c1.CustId);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+
+        }
+    }
+}
