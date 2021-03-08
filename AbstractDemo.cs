8569b15f8217d5cfe69f60af25622ef686466b96
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-08 10:12:54 +0530

    Add files via upload

---
+++ AbstractDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLibrary;
+namespace OOPs
+{
+    class AbstractDemo
+    {
+        static void Main(string[] args)
+        {
+            //GeoShapes shapes = new GeoShapes();
+            Circle circle = new Circle();
+            double Circlearea=circle.CalculateArea(radius: 3);
+            Console.WriteLine(Circlearea);
+
+            Triangle t = new Triangle();
+            t.Height = 12;
+            double triArea=t.CalculateArea(height:t.Height,width: 3);
+            Console.WriteLine(triArea);
+
+            Console.Read();
+        }
+    }
+}
---
+++ GeoShapes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+
+   public abstract class GeoShapes
+    {
+        public int Length { get; set; }
+        public int Height { get; set; }
+
+        public  void ShowInfoOfShapes()
+        {
+            Console.WriteLine("cirlce has radius");
+            Console.WriteLine("Triangle has height and base");
+            Console.WriteLine("Rectangle has height and width");
+        }
+
+        public abstract double CalculateArea(int length=0,int width=0,int height=0,int radius=0);
+
+
+      
+    }
+
+   public class Circle : GeoShapes
+    {
+        public override sealed double CalculateArea(int length = 0, int width = 0, int height = 0, int radius = 0)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    public class Triangle : GeoShapes
+    {
+        public override double CalculateArea(int length = 0, int width = 0, int height = 0, int radius = 0)
+        {
+            const float i = 0.5f;
+            return i * width * height;
+        }
+    }
+}
