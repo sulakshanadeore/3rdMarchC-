63bf3b408921e03326795a4bb2737dbe67df7b84
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-19 11:04:23 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartialViewsExceptionHandingDemo.Models
+{
+    public class Class1
+    {
+
+        public int firstnumber { get; set; }
+
+        public int secondnumber { get; set; }
+
+        public int Addition { get { return firstnumber + secondnumber; } }
+
+
+        public int subtraction { get { return firstnumber - secondnumber; } }
+    }
+}
