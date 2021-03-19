1808ef44f6940797b8f00fec26f52d69fe0d005a
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-19 16:39:08 +0530

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstCoreMVCWebApplication.Models;
+using FirstCoreMVCWebApplication.DTO;
+namespace FirstCoreMVCWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+        private readonly IConfiguration config;
+        public StudentController(IConfiguration config1)
+        {
+            config = config1;
+        }
+        public IActionResult Index()
+        {
+
+            Student model = new Student();
+
+            StudentDTO dto = new StudentDTO();
+            model.Students = dto.GetStudents();
+
+            return View(model.Students);
+            
+        }
+    }
+}
---
+++ StudentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstCoreMVCWebApplication.Models;
+namespace FirstCoreMVCWebApplication.DTO
+{
+    public class StudentDTO
+    {
+        static List<Student> studs = new List<Student>();
+
+        public List<Student> GetStudents()
+        {
+
+            studs.Add(new Student {FirstName="A",LastName="A",Mobile="22234",Email="a@gmail.com" });
+
+            studs.Add(new Student { FirstName = "B", LastName = "B", Mobile = "33334", Email = "b@gmail.com" });
+
+            studs.Add(new Student { FirstName = "C", LastName = "C", Mobile = "66634", Email = "c@gmail.com" });
+
+            return studs;
+        }
+    }
+}
