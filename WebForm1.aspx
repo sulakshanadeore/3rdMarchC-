573910612910bf5e575eccbda12180c66d1a302d
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-24 15:23:06 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+            <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" style="height: 26px" />
+            <asp:Button ID="Button4" runat="server" Text="Find" OnClick="Button4_Click" />
+            <asp:Button ID="Button5" runat="server" Text="Show" OnClick="Button5_Click" />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NWLibrary;
+using System.Data.Linq;
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        NorthwindContextDataContext context = new NorthwindContextDataContext();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //int i=context.AddEmployee("Humpty", "Dumpty");
+            //context.SubmitChanges();
+            Employee emp = new Employee();
+            emp.FirstName = "Akanksha";
+            emp.LastName = "Singh";
+
+            context.Employees.InsertOnSubmit(emp);
+            context.SubmitChanges();
+
+
+        }
+
+
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //Employee emp = context.Employees.FirstOrDefault(s => s.EmployeeID == 14);
+            string s= "Akanksha";
+            //Employee emp = context.Employees.Where(s => s.FirstName == "Akanksha").LastOrDefault()
+           int cnt = context.Employees.Where(s1 => s1.FirstName == s).Count();
+            Employee e1 = context.Employees.Where(s1 => s1.FirstName == s).LastOrDefault();
+            Response.Write(e1.City);
+
+            //emp.FirstName = "Humpty";
+            //emp.LastName = "Dumpty";
+
+
+            context.SubmitChanges();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Employee emp = context.Employees.FirstOrDefault(s => s.EmployeeID == 14);
+            context.Employees.DeleteOnSubmit(emp);
+            context.SubmitChanges();
+
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Employee emp = context.Employees.FirstOrDefault(s => s.EmployeeID == 13);
+            Response.Write(emp.EmployeeID + " " + emp.FirstName + " " + emp.LastName);
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            Table<Employee> emptab=context.Employees;
+            GridView1.DataSource = emptab;
+            GridView1.DataBind();
+        }
+    }
+}
