8b54cde94a6720089a84e6e85fa6af125a410a3e
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-26 15:48:05 +0530

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        int Add(int i, int j);
+
+        [OperationContract]
+       List<Employees> GetDataEmployees();
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Employees
+    {
+        private int _empid;
+            [DataMember]
+        public int Empid
+        {
+            get { return _empid; }
+            set { _empid = value; }
+        }
+
+        private string _fname;
+        [DataMember]
+        public string Fname
+        {
+            get { return _fname; }
+            set { _fname = value; }
+        }
+
+        private string _lname;
+
+        public string LName
+        {
+            get { return _lname; }
+            set { _lname = value; }
+        }
+
+
+
+
+    }
+}
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ServiceClientTrial.ServiceReference1;
+namespace ServiceClientTrial
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client proxy = new Service1Client();
+            Employees[] emp = new Employees[] { };
+            emp=proxy.GetDataEmployees();
+            List<Employees> empdata = new List<Employees>();
+            empdata = emp.ToList<Employees>();
+            GridView1.DataSource = empdata;
+            GridView1.DataBind();
+
+            foreach (var item in empdata)
+            {
+                ListBox1.Items.Add(item.Empid + "  " + item.Fname);
+            }
+
+        }
+    }
+}
