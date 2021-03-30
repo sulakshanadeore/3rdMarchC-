ab0f52421e7816a8b35a98d43ed16f4ec6d3885b
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-03-30 15:36:19 +0530

    Add files via upload

---
+++ CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallapiInMVCApp.Models
+{
+    public class CustomerViewModel
+    {
+        public string Custid { get; set; }
+        public string CompanyName { get; set; }
+
+        public string City { get; set; }
+    }
+}
