c086d8cd3eefa61bcfc07873ca4ca1ea1acfa97f
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-04-01 11:20:17 +0530

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            StackPanel panel = new StackPanel();
+            this.Content = panel;
+
+            Button b = new Button();
+            b.Content = "Button1";
+            b.HorizontalAlignment = HorizontalAlignment.Left;
+            b.Margin = new Thickness(150);
+            b.VerticalAlignment = VerticalAlignment.Top;
+            b.Width = 75;
+            b.Height = 40;
+            panel.Children.Add(b);
+
+        }
+    }
+}
